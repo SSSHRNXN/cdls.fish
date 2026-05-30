b4325f8125e94bccc362496dae22bf077bf9df9f
SSHRNXN <zmoooo097@gmail.com>
2026-05-30 15:49:57 +0500

    Added colorize ls output and logic if dir

--- functions/cdls.fish
+++ functions/cdls.fish
@@ -4,13 +4,29 @@ function cdls
 
 	set -l "CLEAN_TTY" "True"
 
-	set -l c (set_color cyan)
-	set -l br (set_color -b red)
-	set -l bc (set_color -b cyan)
-	set -l nc (set_color normal)
-	set -l rc (set_color -r)
+	set -g c (set_color cyan)
+	set -g b (set_color blue --bold --underline)
+	set -g br (set_color -b red)
+	set -g bc (set_color -b cyan)
+	set -g nc (set_color normal)
+	set -g rc (set_color -r)
 
 	set -l values_for_printf "│ %-14s │ %-10s │ %-10s │ %-16s │ %s\n"
+
+	function ls_for_cdls
+		set -l values_for_printf "│ %-14s │ %-10s │ %-10s │ %-16s │ %s\n"
+	
+		#$1 = permissins $3=userowner $4=groupowner date=dateofcreation $8=file/directory
+		ls -la --time-style=long-iso | awk \
+			-v values="$values_for_printf" \
+			-v blue="$b" \
+			-v cyan="$c" '
+			NR>1 {
+				date = $6 " " $7 
+				colorize_if_dir = substr($1,1,1) == "d" ? cyan : blue
+				printf values, $1, $3, $4, date, colorize_if_dir $8 "\033[0m"
+			}'
+	end
 
 	set -l file_dir (dirname $argv)
 
@@ -51,21 +67,20 @@ function cdls
 	end
 
    	cd $argv 2>/dev/null
-	#ls -la --time-style=long-iso | awk -v values="$values_for_printf" 'NR>1 {date = $6 " " $7; printf values, $1, $3, $4, date, "\033[36m" $8 "\033[0m"}'
 
 	echo "$PWD"
 	printf "$rc$values_for_printf" "permissions" "userown" "groupown" "date created" "file $nc"
 	
-	ls -la --time-style=long-iso | head -n 35 | awk -v values="$values_for_printf" 'NR>1 {date = $6 " " $7; printf values, $1, $3, $4, date, "\033[36m" $8 "\033[0m"}'
+	ls_for_cdls | head -n 45
 	
 	set -l counter_of_files (ls -la | wc -l)
-	if [ "$counter_of_files" -gt 35 ]
-		set -l value_of_contain_files (math "$counter_of_files-35")
+	if [ "$counter_of_files" -gt 45 ]
+		set -l value_of_contain_files (math "$counter_of_files-45")
 		read -P "$rc Dir contains $value_of_contain_files files more, show them all? [y/n]:$nc" show_all_files
 			switch $show_all_files
 			case "y"
 				printf '\033[1A\033[2K\r' 
-				ls -la --time-style=long-iso | tail -n +35 | awk -v values="$values_for_printf" 'NR>1 {date = $6 " " $7; printf values, $1, $3, $4, date, "\033[36m" $8 "\033[0m"}'
+				ls_for_cdls | tail -n +45 
 			case "*"
 				printf '\033[1A\033[2K\r' 
 			end
